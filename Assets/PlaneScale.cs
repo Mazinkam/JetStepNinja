dda3ac1fa57b719d925d7d919513c6dd801db9c7
Ali Abdul-Karim <ali.abdulkarim90@gmail.com>
2014-06-16 20:51:08 +0300

    wip menu

---
+++ Assets/PlaneScale.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScale : MonoBehaviour {
+
+    private float planeWidth;
+    private float planeHeight;
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+
+        float worldScreenHeight = Screen.height/13.5f;
+        float worldScreenWidth = Screen.width/13.5f;
+
+        Vector3 xWidth = transform.localScale;
+        xWidth.x = worldScreenWidth / width ;
+        transform.localScale = xWidth;
+        // transform.localScale.x = worldScreenWidth / width;
+        Vector3 yHeight = transform.localScale;
+        yHeight.y = worldScreenHeight / height ;
+        transform.localScale = yHeight;
+       // transform.localScale.y = worldScreenHeight / height;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/RunnerMainMenu.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerMainMenu : MonoBehaviour
+{
+
+    public GameObject waterRun;
+    private Animator animator;
+
+    private float speed;
+    private float splashFix = 0;
+
+    private float runnerSpeed;
+
+    void Start()
+    {
+        GameEventManager.GameStart += GameStart;
+        GameEventManager.GameOver += GameOver;
+        renderer.enabled = false;
+        rigidbody.isKinematic = false;
+        enabled = true;
+        animator = GetComponent<Animator>();
+        animator.speed = 2f;
+
+    }
+
+    void Update()
+    {
+
+        speed = Mathf.Clamp(1 + Time.deltaTime, -1f, 1f);
+        animator.SetFloat("Speed", speed);
+
+    }
+
+    void FixedUpdate()
+    {
+
+      animator.speed = 2f;
+
+      if (splashFix > 0.1)
+      {
+          Splash(transform.position.x, 10);
+          splashFix = 0;
+      }
+      else
+          splashFix += Time.deltaTime;
+
+
+    }
+
+
+    public void Splash(float xpos, float velocity)
+    {
+        //Set the lifetime of the particle system.
+        float lifetime = 0.93f + Mathf.Abs(velocity) * 0.07f;
+
+        //Set the splash to be between two values in Shuriken by setting it twice.
+        waterRun.GetComponent<ParticleSystem>().startSpeed = 8 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
+        waterRun.GetComponent<ParticleSystem>().startSpeed = 9 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
+        waterRun.GetComponent<ParticleSystem>().startLifetime = lifetime;
+
+        //Set the correct position of the particle system.
+        Vector3 position = new Vector3(xpos, transform.position.y - 2, 0);
+
+        //This line aims the splash towards the middle. Only use for small bodies of water:
+        Quaternion rotation = Quaternion.LookRotation(new Vector3(xpos, 1 + 8, 5) - position);
+
+        //Create the splash and tell it to destroy itself.
+        GameObject tempObj = Instantiate(waterRun, position, rotation) as GameObject;
+        Destroy(tempObj, lifetime / 2);
+
+    }
+
+
+
+
+
+    private void GameStart()
+    {
+ 
+
+        renderer.enabled = true;
+        // rigidbody.velocity = Vector3.zero;
+
+        enabled = true;
+        animator.SetFloat("Speed", 0);
+  
+
+    }
+
+    private void GameOver()
+    {
+        renderer.enabled = false;
+        rigidbody.isKinematic = true;
+        enabled = false;
+ 
+    }
+}
