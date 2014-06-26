d78f21faceaa2f943830fd85cd3b9830f0b0ed1b
Marko <marko.hamppula@gmail.com>
2014-06-26 18:37:15 +0300

    Rotate trigger added

---
+++ Assets/Runner/RotateTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTrigger : MonoBehaviour {
+
+	Runner player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("CuteMolly").GetComponent<Runner>();
+	}
+
+	void OnTriggerEnter(Collider obj)
+	{
+		// CuteMolly -> Bip001
+		// Tag Player
+		if(obj.gameObject.tag == "Player")
+		{
+			Debug.Log("Rotate Me");
+			player.startRotateMe();
+		}
+	}
+}
