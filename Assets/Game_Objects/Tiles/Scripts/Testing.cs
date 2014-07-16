606a4e3d5e1287c8985ff63202c4cbbef0f377a7
Ali Abdul-Karim <ali.abdulkarim90@gmail.com>
2014-07-16 03:30:46 +0300

    Paralax for forest level

---
+++ Assets/Game_Objects/Tiles/Scripts/Testing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testing : MonoBehaviour {
+
+	public int materialIndex = 0;
+	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
+	public string textureName = "forest1";
+	
+	Vector2 uvOffset = Vector2.zero;
+	
+	void LateUpdate()
+	{
+		uvOffset += ( uvAnimationRate * Time.deltaTime );
+		if( renderer.enabled )
+		{
+			renderer.materials[ materialIndex ].SetTextureOffset( textureName, uvOffset );
+		}
+	}
+
+
+}
