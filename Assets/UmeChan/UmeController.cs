0e0208221ba4dcdcca7bf6e9c2fb25b83c43ca8c
Ali Abdul-Karim <ali.abdulkarim90@gmail.com>
2014-09-06 01:53:56 +0300

    changed names of some particle effects and fixed some. Fixed UmeController script and cleaned up the code

--- Assets/UmeChan/UmeController.cs
+++ Assets/UmeChan/UmeController.cs
@@ -3,38 +3,29 @@ using System.Collections;
 
 public class UmeController : MonoBehaviour
 {
-	public Camera curCam;
-	
 	public Vector3 jumpVelocity, landVelocity;
-	public float gameOverY;
 	public GameObject waterRun, waterJump, airJump, airJumpUpside;
-	
 	public float runnerSpeed;
-	
 	public static Transform UmeTransform;
 	
-	private bool touchingPlatform;
-	private Vector3 startPosition;
+	private bool touchingPlatform, goingDown;
 	private Animator animator;
 	private float speed;
-	private bool goingDown;
-	private float splashFix = 0;
-	
+	private float particleFix = 0;
 	private bool canRotate = false;
+	private bool reversePhysics = false;
 	private bool gameMode2 = false;
 	private Transform floor, roof;
 	
 	void Start()
 	{
-		startPosition = transform.localPosition;
-	//	renderer.enabled = false;
 		rigidbody.isKinematic = false;
 		enabled = true;
 		
 		animator = GetComponent<Animator>();
 		animator.speed = 10f;
 		
-	//	floor = GameObject.Find("BottomFloorSpawner").transform;
+		//	floor = GameObject.Find("BottomFloorSpawner").transform;
 		
 		if(GameObject.FindGameObjectWithTag("GameMode2") != null)
 		{
@@ -45,7 +36,26 @@ public class UmeController : MonoBehaviour
 	
 	void Update()
 	{
-		// Check if player outside
+		UpdateGameMode2();
+		UpdateJumping();
+		
+		
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		
+		
+		speed = Mathf.Clamp(runnerSpeed + Time.deltaTime, -1f, 1f);
+		animator.SetFloat("Speed", speed);
+		
+		UpdateMoveParticles();
+		
+		
+	}
+	
+	void UpdateGameMode2()
+	{
 		if(gameMode2)
 		{
 			if(roof.position.y < transform.position.y)
@@ -57,7 +67,10 @@ public class UmeController : MonoBehaviour
 				transform.position = new Vector3(transform.position.x, floor.position.y - 0.2f, transform.position.z);
 			}
 		}
-		
+	}
+	
+	void UpdateJumping ()
+	{
 		if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
 		{
 			if (touchingPlatform)
@@ -75,6 +88,7 @@ public class UmeController : MonoBehaviour
 					if(Physics.gravity.y < 0f)
 					{
 						animator.SetBool("UpsideRun",true);
+						reversePhysics = true;
 						Physics.gravity *= -1;
 						jumpVelocity *= -1;
 						landVelocity *= -1;
@@ -82,6 +96,7 @@ public class UmeController : MonoBehaviour
 					else
 					{
 						animator.SetBool("UpsideRun",false);
+						reversePhysics = false;
 						Physics.gravity *= -1;
 						jumpVelocity *= -1;
 						landVelocity *= -1;
@@ -92,48 +107,22 @@ public class UmeController : MonoBehaviour
 				goingDown = true;
 				
 				if(gameMode2 && Physics.gravity.y < 0f)
-					AirSplash(transform.position.x, 10, true);
+					ParticleCreationJump();
 				else
-					AirSplash(transform.position.x, 10);
+					ParticleCreationJump();
 			}
 		}
-		/*
-		if (!touchingPlatform)
+	}
+	
+	void UpdateMoveParticles ()
+	{
+		if (touchingPlatform && particleFix >= 0.2)
 		{
-			Vector3 camCurVec = curCam.transform.position;
-			float wantedZoom = camCurVec.z - 90;
-          
-			if (wantedZoom > -190)
-				camCurVec = Vector3.Lerp(camCurVec, new Vector3(camCurVec.x, camCurVec.y, wantedZoom), Time.deltaTime);
-
-			curCam.transform.position = camCurVec;
+			ParticleCreationRun();
+			particleFix = 0;
 		}
-
-		if (touchingPlatform)
-		{
-			Vector3 camCurVec = curCam.transform.position;
-			float wantedZoom = camCurVec.z + 90;
-         
-			if (wantedZoom < 39)
-				camCurVec = Vector3.Lerp(camCurVec, new Vector3(camCurVec.x, camCurVec.y, wantedZoom), Time.deltaTime);
-
-			curCam.transform.position = camCurVec;
-		}*/
-		
-		if (Input.GetKey(KeyCode.Escape))
-		{
-			Application.Quit();
-		}
-		
-		
-		speed = Mathf.Clamp(runnerSpeed + Time.deltaTime, -1f, 1f);
-		animator.SetFloat("Speed", speed);
-		
-		if (transform.localPosition.y < gameOverY)
-		{
-			//	GameEventManager.TriggerGameOver();
-		}
-		
+		else
+			particleFix += Time.deltaTime;
 	}
 	
 	void FixedUpdate()
@@ -154,79 +143,67 @@ public class UmeController : MonoBehaviour
 		
 	}
 	
-	public void Splash(float xpos, float velocity)
+	public void ParticleCreationRun ()
 	{
 		//Set the lifetime of the particle system.
-		float lifetime = 0.93f + Mathf.Abs(velocity) * 0.07f;
+		float lifetime = 0.93f  + Mathf.Abs(speed)  * 0.07f;
 		
-		//Set the splash to be between two values in Shuriken by setting it twice.
-		waterRun.GetComponent<ParticleSystem>().startSpeed = 8 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
-		waterRun.GetComponent<ParticleSystem>().startSpeed = 9 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
-		waterRun.GetComponent<ParticleSystem>().startLifetime = lifetime;
+		GameObject tempParticleSystem;
+		
+		if(touchingPlatform)
+			tempParticleSystem = waterRun;
+		else
+			tempParticleSystem = waterJump;
+		
+
+		//Set the correct position of the particle system.
+		Vector3 position;
+		if(gameMode2 && Physics.gravity.y > 0)
+			position = new Vector3(UmeTransform.position.x+3, UmeTransform.position.y-0.5f, UmeTransform.position.z+2);
+		else
+			position = new Vector3(UmeTransform.position.x-3, UmeTransform.position.y-0.5f, UmeTransform.position.z+2);
+
+		
+		GameObject tempSplashObj;
+		//Create the splash and tell it to destroy itself.
+		if(gameMode2 && !reversePhysics)
+			tempSplashObj = Instantiate(tempParticleSystem, position, Quaternion.identity) as GameObject;
+		else
+			tempSplashObj = Instantiate(tempParticleSystem, position, Quaternion.identity) as GameObject;
+		
+
+		Destroy(tempSplashObj, tempSplashObj.particleSystem.startLifetime);
+		
+	}
+	
+	public void ParticleCreationJump ()
+	{
+		//Set the lifetime of the particle system.
+		float lifetime = 0.93f  + Mathf.Abs(speed) * 0.07f;
+		
+		GameObject tempParticleSystem;
+		
+		if(gameMode2 && !reversePhysics)
+			tempParticleSystem = airJumpUpside;
+		else
+			tempParticleSystem = airJump;;
 		
 		//Set the correct position of the particle system.
 		Vector3 position;
 		if(gameMode2 && Physics.gravity.y > 0)
-			position = new Vector3(xpos, transform.position.y + 2, 0);
+			position = new Vector3(UmeTransform.position.x+3, UmeTransform.position.y-0.5f, UmeTransform.position.z+2);
 		else
-			position = new Vector3(xpos, transform.position.y - 2, 0);
+			position = new Vector3(UmeTransform.position.x-3, UmeTransform.position.y-0.5f, UmeTransform.position.z+2);
+
 		
-		//This line aims the splash towards the middle. Only use for small bodies of water:
-		Quaternion rotation = Quaternion.LookRotation(new Vector3(xpos, 1 + 8, 5) - position);
-		
-		//Create the splash and tell it to destroy itself.
-		GameObject tempSplashObj = Instantiate(waterRun, position, rotation) as GameObject;
-		
-		
-		Destroy(tempSplashObj, lifetime / 2);
-		
-	}
-	
-	public void JumpSplash(float xpos, float velocity)
-	{
-		//Set the lifetime of the particle system.
-		float lifetime = 0.93f + Mathf.Abs(velocity) * 0.07f;
-		
-		//Set the splash to be between two values in Shuriken by setting it twice.
-		waterJump.GetComponent<ParticleSystem>().startSpeed = 8 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
-		waterJump.GetComponent<ParticleSystem>().startSpeed = 9 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
-		waterJump.GetComponent<ParticleSystem>().startLifetime = lifetime;
-		
-		//Set the correct position of the particle system.
-		Vector3 position = new Vector3(xpos, transform.position.y - 2, 0);
-		
-		//This line aims the splash towards the middle. Only use for small bodies of water:
-		Quaternion rotation = Quaternion.LookRotation(new Vector3(xpos, 1 + 8, 5) - position);
-		
-		//Create the splash and tell it to destroy itself.
-		GameObject tempSplashObj = Instantiate(waterRun, position, rotation) as GameObject;
-		Destroy(tempSplashObj, lifetime / 2);
-		
-	}
-	
-	public void AirSplash(float xpos, float velocity, bool upside = false)
-	{
-		//Set the lifetime of the particle system.
-		float lifetime = 0.93f + Mathf.Abs(velocity) * 0.07f;
-		
-		//Set the splash to be between two values in Shuriken by setting it twice.
-		airJump.GetComponent<ParticleSystem>().startSpeed = 8 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
-		airJump.GetComponent<ParticleSystem>().startSpeed = 9 + 2 * Mathf.Pow(Mathf.Abs(velocity), 0.5f);
-		airJump.GetComponent<ParticleSystem>().startLifetime = lifetime;
-		
-		//Set the correct position of the particle system.
-		Vector3 position = new Vector3(xpos, transform.position.y - 2, 0);
-		
-		//This line aims the splash towards the middle. Only use for small bodies of water:
-		// Quaternion rotation = Quaternion.LookRotation(new Vector3(xpos, 1 + 8, 5) - position);
-		
-		//Create the splash and tell it to destroy itself.
 		GameObject tempSplashObj;
-		if(gameMode2 && !upside)
-			tempSplashObj = Instantiate(airJumpUpside, position, Quaternion.identity) as GameObject;
+		//Create the splash and tell it to destroy itself.
+		if(gameMode2 && !reversePhysics)
+			tempSplashObj = Instantiate(tempParticleSystem, position, Quaternion.identity) as GameObject;
 		else
-			tempSplashObj = Instantiate(airJump, position, Quaternion.identity) as GameObject;
-		Destroy(tempSplashObj, lifetime / 2);
+			tempSplashObj = Instantiate(tempParticleSystem, position, Quaternion.identity) as GameObject;
+
+		Destroy(tempSplashObj, tempSplashObj.particleSystem.startLifetime);
 		
 	}
 	
@@ -235,7 +212,7 @@ public class UmeController : MonoBehaviour
 		canRotate = true;
 		touchingPlatform = true;
 		animator.SetBool("Landed", true);
-		//Splash(transform.position.x, runnerSpeed);
+		ParticleCreationRun();
 		//	if (other.collider.tag == "Smelly")
 		
 	}
@@ -243,36 +220,20 @@ public class UmeController : MonoBehaviour
 	void OnCollisionStay()
 	{
 		touchingPlatform = true;
-		
-		// gameObject.rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
-		if (splashFix > 0.2)
-		{
-			Splash(transform.position.x, runnerSpeed);
-			splashFix = 0;
-		}
-		else
-			splashFix += Time.deltaTime;
 		animator.SetBool("Landed", false);
-
+		
 	}
 	
 	void OnCollisionExit()
 	{
 		touchingPlatform = false;
-		
-		JumpSplash(transform.position.x, 10);
+		ParticleCreationRun();
 		animator.SetBool("Landed", false);
 	}
 	
 	private void GameStart()
 	{
-		transform.localPosition = startPosition;
-		renderer.enabled = true;
-		// rigidbody.velocity = Vector3.zero;
-		
 		rigidbody.isKinematic = false;
-		// rigidbody.velocity = new Vector3(15, 0, 0);
-		
 		enabled = true;
 		animator.SetFloat("Speed", 0);
 		
